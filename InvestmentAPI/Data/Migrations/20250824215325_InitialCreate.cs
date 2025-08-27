afd0ce75e4223748e2744f5598b65a302d942788
Chatri Ngambenchawong <pingkunga@gmail.com>
2025-08-27 23:03:22 +0700

    feat: add initial script

---
+++ InvestmentAPI/Data/Migrations/20250824215325_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvestmentAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DataSource = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Timestamp = table.Column<long>(type: "bigint", nullable: false),
+                    MTMDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    BaseCurrency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    Currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    Rate = table.Column<decimal>(type: "numeric(18,6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_MTMDate_BaseCurrency_Currency",
+                table: "ExchangeRates",
+                columns: new[] { "MTMDate", "BaseCurrency", "Currency" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExchangeRates");
+        }
+    }
+}
